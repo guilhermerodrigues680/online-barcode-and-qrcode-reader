ea830baf44f7faf78cd10b0166485e409a3bb2eb
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2021-05-07 07:48:07 -0300

    Build - Add Prerender

--- docs/js/result.35f3408f.js
+++ docs/js/result.35f3408f.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["result"],{eeac:function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"result"},[n("h1",[e._v("This is an result page")])])}],r=n("2877"),a={},u=Object(r["a"])(a,s,l,!1,null,null,null);t["default"]=u.exports}}]);
-//# sourceMappingURL=result.1747b2a2.js.map
+//# sourceMappingURL=result.35f3408f.js.map
